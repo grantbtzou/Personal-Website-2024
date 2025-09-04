149de8601886d747f5ef7df082038eeafc05020f
Grant Zou <grantbtzou@gmail.com>
2025-09-03 23:31:51 -0400

    added scaffolding for new project

--- src/app/components/simulation.js
+++ src/app/components/simulation.js
@@ -5,7 +5,9 @@ export default function Simulation({Path, ImageUrl, Title, Description}){
         <a href={Path}>
         <div className="flex flex-wrap flex-col bg-slate-200 max-w-72 h-96  rounded-lg overflow-hidden shadow-2xl">
             <div className="flex flex-wrap justify-center w-full border-b-2">
-            <Image src={ImageUrl} height={150} width={288}/></div>
+            <div className="relative w-[288px] h-[150px]">
+              <Image className="object-cover" src={ImageUrl} fill/></div>
+            </div>
             <div className="px-4 mt-4">
             <h1 className="text-3xl text-start">{Title}</h1>
             <p className="text-xl">{Description}</p>
