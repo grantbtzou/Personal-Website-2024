aaef6a50f505c11ecde1162b211cb2cb8f54e449
Grant Zou <grantbtzou@gmail.com>
2026-04-09 23:52:39 -0400

    space log out nicely

--- src/app/Simulations/WebSocketTest/game/[id]/GraphGame/GameLog/gameLog.js
+++ src/app/Simulations/WebSocketTest/game/[id]/GraphGame/GameLog/gameLog.js
@@ -6,27 +6,49 @@ function GameLog(){
     dispatch({ type: 'SET_VIEWING_TURN', payload: { viewingTurn: index + 1 }});
   };
 
-  return(
-    <div className="border p-4 w-48 overflow-y-auto h-[500px]">
-      <h2>Game Log</h2>
-      {state.match.log.map((logEntry, index) => {
-        const attacks = logEntry.resolution.attacks;
-        const player1Attack = attacks.find(attack => attack.attacker === 'player1');
-        const player2Attack = attacks.find(attack => attack.attacker === 'player2');
-        const isViewing = state.match.viewingTurn === index + 1;
-        return( 
-          <button
-            key={index}
-            className={`border p-2 my-2 w-full text-left ${isViewing ? "bg-blue-100 border-blue-400" : ""}`}
-            onClick={() => handleTurnClick(index)}
-          >
-          <p>Turn {logEntry.turnNumber}:</p>
-          <p className="whitespace-nowrap">P1: {player1Attack?.targetId}</p>
-          <p className="whitespace-nowrap">P2: {player2Attack?.targetId}</p>
-          </button>)
-      }
-      )}
+  return (
+  <div className="border w-48 overflow-y-auto h-[500px]">
+    <h2>Game Log</h2>
+    {/* Header row */}
+    <div className="flex flex-row">
+      <div className="w-6" /> {/* spacer for turn number */}
+      <div className="flex flex-row flex-1">
+        <p className="w-1/2 text-center">P1</p>
+        <p className="w-1/2 text-center">P2</p>
+      </div>
     </div>
-  )}
+
+    {state.match.log.map((logEntry, index) => {
+      const player1Attack = logEntry.selections.player1.attack;
+      const player2Attack = logEntry.selections.player2.attack;
+      const player1Defend = logEntry.selections.player1.defend;
+      const player2Defend = logEntry.selections.player2.defend;
+      const isViewing = state.match.viewingTurn === index + 1;
+      return (
+        <button
+          key={index}
+          className={`border-y w-full text-left ${isViewing ? "bg-blue-100 border-blue-400" : ""}`}
+          onClick={() => handleTurnClick(index)}
+        >
+          <div className="flex flex-row">
+            <div className="w-6"><p>{logEntry.turnNumber}</p></div>
+            <div className="flex-1">
+              <div className="flex flex-row">
+                <div><p>A</p></div>
+                <p className="w-1/2 text-center">{player1Attack}</p>
+                <p className="w-1/2 text-center">{player2Attack}</p>
+              </div>
+              <div className="flex flex-row">
+                <div><p>D</p></div>
+                <p className="w-1/2 text-center">{player1Defend}</p>
+                <p className="w-1/2 text-center">{player2Defend}</p>
+              </div>
+            </div>
+          </div>
+        </button>
+      );
+    })}
+  </div>
+);}
 
 export default GameLog;
